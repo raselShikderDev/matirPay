1b2430b0cdb4be8284f45051a0607271fc2f3309
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-09-07 01:15:02 +0600

    chnages order of middleware for cookies

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -1,25 +1,25 @@
 import { Response } from "express";
 
 interface authCookie {
-    accessToken?:string,
-    refreshToken?:string,
+  accessToken?: string;
+  refreshToken?: string;
 }
 
-const setCookies = async(res:Response, tokens:authCookie)=>{
-    if (tokens.accessToken) {
-        res.cookie("accessToken", tokens.accessToken,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none"
-    })
-    }
-    if (tokens.refreshToken) {
-        res.cookie("refreshToken", tokens.refreshToken,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none"
-    })
-    }
-}
+const setCookies = async (res: Response, tokens: authCookie) => {
+  if (tokens.accessToken) {
+    res.cookie("accessToken", tokens.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+  }
+  if (tokens.refreshToken) {
+    res.cookie("refreshToken", tokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+  }
+};
 
-export default setCookies
+export default setCookies;
